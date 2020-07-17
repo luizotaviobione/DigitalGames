ff2cb82e1af5ac1389051a505e795816e7ca8b95
luizotaviobione <luix195@gmail.com>
2020-07-17 00:14:01 -0300

    itens conseguem ser adicionado ao carrinho e valores conseguem ser atualizados

---
+++ app.js
@@ -0,0 +1,161 @@
+/*variaveis*/
+secaoJogos = document.querySelector(".jogos");
+efeitoCarrinho = document.querySelector(".efeito-carrinho");
+carrinho = document.querySelector(".carrinho");
+fecharJanelaCarrinho = document.querySelector(".botao-carrinho");
+elementosCarrinho = document.querySelector(".conteudo-carrinho");
+botaoCarrinho = document.querySelector(".carrinho-nav");
+precoTotal = document.querySelector(".preco-total");
+
+var produtosCarrinho = [];
+
+const arrayJogos = [
+  {
+    nome: "Grand theft auto - Ps4",
+    img: "images/jogos/gta.png",
+    preco: 19.95,
+  },
+  {
+    nome: "Bully",
+    img: "images/jogos/bully.png",
+    preco: 15.35,
+  },
+  {
+    nome: "Star Wars Battlefront",
+    img: "images/jogos/battlefront.jpeg",
+    preco: 12.45,
+  },
+  {
+    nome: "Uncharted The Lost Legacy",
+    img: "images/jogos/uncharted.jpg",
+    preco: 12.43,
+  },
+  {
+    nome: "Minecraft",
+    img: "images/jogos/minecraft.jpg",
+    preco: 10.42,
+  },
+  {
+    nome: "Crash Bandicoot",
+    img: "images/jogos/crash.jpg",
+    preco: 13.22,
+  },
+];
+
+class Produtos {
+  mostrarProdutos() {
+    var i = 0;
+    var adicionando = "";
+    //console.log(arrayJogos);
+    arrayJogos.forEach((jogo) => {
+      //console.log(jogo);
+      adicionando += `
+      <div class="borda col-md-6 col-lg-4 mt-2 mb-2" align="center" id="${i}">
+          <div class="produto">
+            <img
+              class="mt-4"
+              src=${jogo.img}
+              alt=""
+              width="220px"
+              height="300px" id="${i}"
+            />
+            <button class="botao-item"  id="${i}">
+              <i class="fas fa-shopping-cart"></i>
+              Add to Cart
+            </button>
+          </div>
+
+          <h4 class="formatoh4">${jogo.nome}</h4>
+          <span class="priceitem">${jogo.preco}R$</span>
+      </div>`;
+      i++;
+    });
+
+    secaoJogos.innerHTML = adicionando;
+  }
+
+  clicarCarrinho() {
+    botaoCarrinho.addEventListener("click", () => {
+      this.aparecerCarrinho();
+    });
+  }
+
+  clicarnoProduto() {
+    const itens = document.querySelectorAll(".borda");
+
+    //abrir o carrinho
+    itens.forEach((produto) => {
+      //produto.addEventListener("click", this.adicionarProduto);
+      produto.addEventListener("click", (e) => {
+        // "";
+        var click = e.target;
+        var jogo = arrayJogos[click.id];
+        produtosCarrinho.push(jogo);
+
+        this.aparecerCarrinho();
+        this.updatevalores();
+        this.adicionarProCarrinho(jogo, click.id);
+      });
+    });
+  }
+
+  updatevalores() {
+    let preco = 0;
+    produtosCarrinho.map((produto) => {
+      preco += produto.preco;
+    });
+    //console.log(precoTotal.innerText);
+    preco = parseFloat(preco);
+    precoTotal.innerText = preco.toFixed(2);
+    console.log(precoTotal.innerHTML);
+  }
+
+  adicionarProCarrinho(jogo, id) {
+    var elemento = "";
+    elemento = `
+          <div class="item-carrinho d-flex">
+            <div class="img-item-carrinho">
+              <img src=${jogo.img} width="120" height="160" alt="" />
+            </div>
+
+            <div class="info-item">
+              <h4 class="">${jogo.nome}</h4>
+              <h4 align="left" class="mt-4 mb-4">${jogo.preco}R$</h4>
+              <span class="remove-item" data-id=${id}>Remove</span>
+            </div>
+
+            <div class="ml-auto pr-3 pt-4">
+              <i class="fas fa-chevron-up"></i>
+              <p class="pt-2">1</p>
+              <i class="fas fa-chevron-down"></i>
+            </div>
+          </div>`;
+
+    elementosCarrinho.innerHTML += elemento;
+    var botaoRemoverItem = document.querySelectorAll(".remove-item");
+
+    botaoRemoverItem.forEach((botao) => {
+      botao.addEventListener("click", () => {});
+    });
+  }
+
+  aparecerCarrinho() {
+    efeitoCarrinho.classList.add("showcart");
+    carrinho.classList.add("aparecerCarrinho");
+
+    fecharJanelaCarrinho.addEventListener("click", () => {
+      //console.log(precoTotal.innerText);
+      efeitoCarrinho.classList.remove("showcart");
+      carrinho.classList.remove("aparecerCarrinho");
+      //console.log(precoTotal.innerText);
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const produtos = new Produtos();
+
+  produtos.mostrarProdutos();
+  produtos.clicarnoProduto();
+  produtos.clicarCarrinho();
+});
